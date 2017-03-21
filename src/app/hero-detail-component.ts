d050c0294bb30266200651fc4e9dfe14568aa00b
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-21 10:46:49 +0200

    updated hero-detail

--- src/app/hero-detail-component.ts
+++ src/app/hero-detail-component.ts
@@ -1,7 +1,18 @@
 import { Component, Inout} from '@angular/core';
+import { Hero } from './hero';
 
 @component({
 	selector: 'my-hero-detail',
+	template: `
+		<div *ngIf="hero">
+    			<h2>{{hero.name}} details!</h2>
+    			<div><label>id: </label>{{hero.id}}</div>
+	    		<div>
+	    	  		<label>name: </label>
+	    	  		<input [(ngModel)]="hero.name" placeholder="name"/>
+	    		</div>
+	  	</div>
+	`
 });
 export class HeroDetailComponent {
 

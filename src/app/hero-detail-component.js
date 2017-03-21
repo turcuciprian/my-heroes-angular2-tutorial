4014b73a10693ac21faa69071a6543bd3b436cec
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-21 10:41:41 +0200

    created hero.ts created new component hero-detail-component.ts and hero.ts imported hero.ts into app.component and hero-detail-component.ts renamed hero-detail-component.ts to hero-detail.component.ts

---
+++ src/app/hero-detail-component.js
@@ -0,0 +1,9 @@
+"use strict";
+;
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent() {
+    }
+    return HeroDetailComponent;
+}());
+exports.HeroDetailComponent = HeroDetailComponent;
+//# sourceMappingURL=hero-detail-component.js.map
---
+++ src/app/hero-detail-component.ts
@@ -0,0 +1,8 @@
+import { Component, Inout} from '@angular/core';
+
+@component({
+	selector: 'my-hero-detail',
+});
+export class HeroDetailComponent {
+
+}
